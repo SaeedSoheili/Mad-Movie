e4f4a063c8b11187ae02897fc49a52e4ad76197e
Saeed Soheili <mr.saeed.soheili@gmail.com>
2023-10-10 22:06:01 +0330

    some demos for slider of the home page

--- src/components/pages/home/Slider/Slider.js
+++ src/components/pages/home/Slider/Slider.js
@@ -3,11 +3,24 @@ import "./Slider.css";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import sliderData from "../getRandomSlider";
+
+function generateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 4); // Generates a random number between 0 and 3
+  return randomNumber;
+}
+
+let randomNumberCrated = generateRandomNumber();
 
 export default function Slider() {
   return (
     <div className="slider__container">
-      <img className="slider__image-cover" />
+      <img
+        className="slider__image-cover"
+        style={{
+          backgroundImage: `url('${sliderData[randomNumberCrated].cover}')`,
+        }}
+      />
       <div className="slider__divider"></div>
       <div className="slider__divider"></div>
       <div className="slider__divider"></div>
@@ -18,34 +31,44 @@ export default function Slider() {
         loop
         id="background-video"
       >
-        <source src="./assets/spiderman-trailer.mp4" type="video/mp4" />
+        <source src={sliderData[randomNumberCrated].trailer} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="slider__contents">
-        <h2 className="slider__movie-name">SpiderMan 1</h2>
+        <h2 className="slider__movie-name">
+          {sliderData[randomNumberCrated].title}
+        </h2>
         <h1 className="slider__title">
           Unlimited <span className="slider__title--highlight">Movie</span>, TVs
           Shows, & More.
         </h1>
         <div className="slider__details">
           <div className="slider__details--box1">
-            <div className="slider__box1--age">PG 18</div>
+            <div className="slider__box1--age">
+              {sliderData[randomNumberCrated].age}
+            </div>
             <div className="slider__box1--quality">HD</div>
           </div>
           <div className="slider__details--box2">
-            <p className="slider__box2--category">Romance, Drama</p>
+            <p className="slider__box2--category">
+              {sliderData[randomNumberCrated].category}
+            </p>
           </div>
           <div className="slider__details--box3">
             <CalendarMonthIcon
               fontSize="small"
               className="slider__box3--calender-img"
             />
-            <p className="slider__box3--release-year">2022</p>
+            <p className="slider__box3--release-year">
+              {sliderData[randomNumberCrated].year}
+            </p>
             <AccessTimeIcon
               fontSize="small"
               className="slider__box3--clock-img"
             />
-            <p className="slider__box3--movie-length">120 min</p>
+            <p className="slider__box3--movie-length">
+              {sliderData[randomNumberCrated].time}
+            </p>
           </div>
         </div>
         <button className="slider__button">
---
+++ src/components/pages/home/getRandomSlider.js
@@ -0,0 +1,44 @@
+let sliderData = [
+  {
+    title: "SpiderMan 1",
+    age: "PG 13",
+    quality: "HD",
+    category: "Action, Adventure, Sci-Fi",
+    year: "2002",
+    time: "2h 1m",
+    cover: "./assets/slider/spiderman-1-cover.png",
+    trailer: "./assets/slider/spiderman-1-trailer.mp4",
+  },
+  {
+    title: "ALPHA",
+    age: "PG 13",
+    quality: "HD",
+    category: "Action, Adventure, Drama",
+    year: "2018",
+    time: "1h 36m",
+    cover: "./assets/slider/alpha-cover.jpg",
+    trailer: "./assets/slider/alpha-trailer.mp4",
+  },
+  {
+    title: "The Batman",
+    age: "PG 13",
+    quality: "HD",
+    category: "Action, Crime, Drama",
+    year: "2022",
+    time: "2h 56m",
+    cover: "./assets/slider/batman-cover.jpg",
+    trailer: "./assets/slider/batman-trailer.mp4",
+  },
+  {
+    title: "Barbie",
+    age: "PG 13",
+    quality: "HD",
+    category: "Adventure, Comedy, Fantasy",
+    year: "2023",
+    time: "1h 54m",
+    cover: "./assets/slider/barbie-cover.jpg",
+    trailer: "./assets/slider/barbie-trailer.mp4",
+  },
+];
+
+export default sliderData;
